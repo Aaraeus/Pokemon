66cbae052a42a1e6d4c93c350c713fef5ea0b5bd
Aaraeus <kintesh.fm@googlemail.com>
2018-07-08 17:17:43 +0100

    Added libraries

--- Pokemon Modelling First Attempt.py
+++ Pokemon Modelling First Attempt.py
@@ -1,4 +1,7 @@
-import pandas as pd
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns  # visualization tool
 
 loc = 'C:\\Users\\Kintesh\\Desktop\\code\\kaggle\\pokemon\\'
 
@@ -7,4 +10,9 @@ pokemon = pd.read_csv(loc + "pokemon.csv")
 test_01 = pd.read_csv(loc + "tests.csv")
 
 print(combats.head(n=5))
+
+print(pokemon.info())
+
+list = [1,4,5,6,8,9,11,12,14,15,16,17]
+median = np.median(list)
 
